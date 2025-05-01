a4c71954dbc8e6523e2278c66fab8f1b3b01350d
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2025-05-01 15:46:30 +0100

    v0.0.05

--- Koha/Plugin/Com/PTFSEurope/SAP.pm
+++ Koha/Plugin/Com/PTFSEurope/SAP.pm
@@ -11,7 +11,7 @@ use File::Spec;
 use List::Util qw(min max);
 use Mojo::JSON qw{ decode_json };
 
-our $VERSION  = '0.0.04';
+our $VERSION  = '0.0.05';
 our $metadata = {
     name => 'SAP Finance Integration',
 
