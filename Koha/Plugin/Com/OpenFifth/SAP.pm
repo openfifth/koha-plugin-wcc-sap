1eefb57d6233962de593f696cd99fcb36e53f492
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2025-06-30 14:45:14 +0100

    chore: bump version

--- Koha/Plugin/Com/OpenFifth/SAP.pm
+++ Koha/Plugin/Com/OpenFifth/SAP.pm
@@ -11,14 +11,14 @@ use File::Spec;
 use List::Util qw(min max);
 use Mojo::JSON qw{ decode_json };
 
-our $VERSION = '0.0.12';
+our $VERSION = '0.0.13';
 
 our $metadata = {
     name => 'SAP Finance Integration',
 
     author          => 'Open Fifth',
     date_authored   => '2024-11-15',
-    date_updated    => '2025-06-27',
+    date_updated    => '2025-06-30',
     minimum_version => '24.11.00.000',
     maximum_version => undef,
     version         => $VERSION,
